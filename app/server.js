98ceb589af34552a032d10d3517a7ada0a5a1577
AzraGal <azradrexel@gmail.com>
2022-11-02 09:59:03 -0400

    Set up basic index.html file

--- app/server.js
+++ app/server.js
@@ -3,6 +3,12 @@ let app = express();
 let port = 3000;
 let hostname = "localhost";
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("public/index.html"); 
+});
+
 app.listen(port, hostname, () => {
     console.log(`http://${hostname}:${port}`);
 });
