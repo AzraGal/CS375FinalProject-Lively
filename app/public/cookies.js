3a5ce5fc835d2f8d1f8f74de91a344352a635ee9
josephine828 <114919028+josephine828@users.noreply.github.com>
2022-11-07 10:12:58 -0500

    Moved over cookie code from index.js

---
+++ app/public/cookies.js
@@ -0,0 +1,54 @@
+export let cookieConsent = getCookie("cookie_consent");
+let cookieContainer = document.getElementById("cookieConsentContainer");
+let acceptCookieButton = document.getElementById("acceptCookieButton");
+let declineCookieButton = document.getElementById("declineCookieButton");
+
+function initCookies() {
+    acceptCookieButton.addEventListener("click", acceptCookie);
+    declineCookieButton.addEventListener("click", declineCookie);
+    if (cookieConsent === "") {
+        cookieContainer.style.display = "block";
+    }
+    else {
+        cookieContainer.style.display = "none";
+    }
+}
+
+function acceptCookie() {
+    cookieContainer.style.display = "none";
+    deleteCookie("cookie_consent");
+    setCookie("cookie_consent", true, 30);
+}
+
+function declineCookie() {
+    cookieContainer.style.display = "none";
+}
+
+// The following two methods, getCookie(...) and setCookie(...) are inspired by 
+// https://www.codexworld.com/cookie-consent-popup-with-javascript/
+export function getCookie(name) {
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieArr = decodedCookie.split(';');
+    for (let c of cookieArr) {
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+export function setCookie(name, value, expirationDays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    let expiration = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expiration + ";path=/";
+}
+
+export function deleteCookie(name) {
+    document.cookie = name + "=; max-age=-1;";
+}
+
+initCookies();
