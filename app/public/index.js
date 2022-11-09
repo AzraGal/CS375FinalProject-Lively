8128d18ac1039d19d9704bcaaaecab5cf65e54a6
Tapasya Sharma <t.sharm7377@gmail.com>
2022-11-09 09:24:39 -0500

    Removing maps code

--- app/public/index.js
+++ app/public/index.js
@@ -1,11 +1,3 @@
-let map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 39.95228, lng: -75.16245 },
-        zoom: 8,
-    });
-}
 
 window.initMap = initMap;
 
