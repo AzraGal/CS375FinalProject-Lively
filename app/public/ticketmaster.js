83ddf78edde3d3f54ba664d0286884875e6f3402
Benedict Hofmockel <dubmh324@gmail.com>
2022-11-12 17:13:04 -0500

    I think I know how to use .gitignores now

--- app/public/ticketmaster.js
+++ app/public/ticketmaster.js
@@ -1,6 +1,7 @@
 let tmGetGenreButton = document.getElementById('buttonTicketMasterGenres');
 
 tmGetGenreButton.addEventListener("click", () => {
+    console.log("fetching Genres from TicketMaster");
     fetch('/tmGenres').then((response) => {
         return response.json();
     }).then((body)=>{
