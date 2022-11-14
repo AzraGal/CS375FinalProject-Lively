bb5f036d5d0f561070e12601529c687522fc2b80
josephine828 <114919028+josephine828@users.noreply.github.com>
2022-11-14 09:59:04 -0500

    Integrated Spotify with the artist search

--- app/public/artistSearch.js
+++ app/public/artistSearch.js
@@ -10,24 +10,26 @@ function artistSearch(key) {
             suggestedArtists.removeChild(suggestedArtists.firstChild);
         }
         fetch(`/artistSearch?artist=${artistInput.value}`).then(response => {
-            response.json().then(body => {
-                artistOptionsData = [];
-                suggestedArtists.textContent = "Searching for: " + artistInput.value;
-                for (let i = 0; i < body.length; i++) {
-                    artistOptionsData.push({
-                        id: i,
-                        text: body[i].name
-                    });
-                    let div = document.createElement("div");
-                    div.textContent = artistOptionsData[i].text;
-                    div.className = "suggestedArtist";
-                    div.addEventListener("click", () => {
-                        artistInput.value = artistOptionsData[i].text;
-                    });
-                    suggestedArtists.append(div);
-                }
-                suggestedArtists.style = "block"; 
-            });
+            if (response.status === 200) {
+                response.json().then(body => {
+                    artistOptionsData = [];
+                    suggestedArtists.textContent = "Searching for: " + artistInput.value;
+                    for (let i = 0; i < body.length; i++) {
+                        artistOptionsData.push({
+                            id: i,
+                            text: body[i].name
+                        });
+                        let div = document.createElement("div");
+                        div.textContent = artistOptionsData[i].text;
+                        div.className = "suggestedArtist";
+                        div.addEventListener("click", () => {   
+                            artistInput.value = artistOptionsData[i].text;
+                        });
+                        suggestedArtists.append(div);
+                    }
+                    suggestedArtists.style = "block"; 
+                });
+            }
         });
     }
 }
