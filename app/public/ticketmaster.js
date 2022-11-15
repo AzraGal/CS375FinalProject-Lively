e828576031eb97e42b42b7fbffa1d4b81820a85e
Benedict Hofmockel <dubmh324@gmail.com>
2022-11-14 21:20:42 -0500

    added Get TicketMaster Events Button

--- app/public/ticketmaster.js
+++ app/public/ticketmaster.js
@@ -1,8 +1,18 @@
 let tmGetGenreButton = document.getElementById('buttonTicketMasterGenres');
+let tmGetEventsButton = document.getElementById('buttonTicketMasterEvents');
 
 tmGetGenreButton.addEventListener("click", () => {
     console.log("fetching Genres from TicketMaster");
     fetch('/tmGenres').then((response) => {
+        return response.json();
+    }).then((body)=>{
+        console.log(body);
+    })
+})
+
+tmGetEventsButton.addEventListener("click", () => {
+    console.log("fetching Events from TicketMaster");
+    fetch('/tmEvents').then((response) => {
         return response.json();
     }).then((body)=>{
         console.log(body);
