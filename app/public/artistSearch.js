316487e5655d7e384d9c20f2a4f6328cf61a91d9
josephine828 <114919028+josephine828@users.noreply.github.com>
2022-11-19 12:28:49 -0500

    Added suggested artists using TicketMaster

--- app/public/artistSearch.js
+++ app/public/artistSearch.js
@@ -9,29 +9,52 @@ function artistSearch(key) {
         while (suggestedArtists.firstChild) {
             suggestedArtists.removeChild(suggestedArtists.firstChild);
         }
-        fetch(`/artistSearch?artist=${artistInput.value}`).then(response => {
-            if (response.status === 200) {
-                response.json().then(body => {
-                    artistOptionsData = [];
-                    suggestedArtists.textContent = "Searching for: " + artistInput.value;
-                    for (let i = 0; i < body.length; i++) {
-                        artistOptionsData.push({
-                            id: i,
-                            text: body[i].name
-                        });
-                        let div = document.createElement("div");
-                        div.textContent = artistOptionsData[i].text;
-                        div.className = "suggestedArtist";
-                        div.addEventListener("click", () => {   
-                            artistInput.value = artistOptionsData[i].text;
-                        });
-                        suggestedArtists.append(div);
-                    }
-                    suggestedArtists.style = "block"; 
-                });
-            }
-        });
+        if (document.getElementById("user-profile").childElementCount !== 0) {
+            artistSearchSpotify();
+        }
+        else {
+            artistSearchTicketMaster();
+        }
     }
+}
+
+function artistSearchSpotify() {
+    fetch(`/artistSearchSpotify?artist=${artistInput.value}`).then(response => {
+        if (response.status === 200) {
+            response.json().then(body => {
+                populateSuggestedArtistsList(body);
+            });
+        }
+    });
+}
+
+function artistSearchTicketMaster() {
+    fetch(`/artistSearchTicketMaster?artist=${artistInput.value}`).then(response => {
+        if (response.status === 200) {
+            response.json().then(body => {
+                populateSuggestedArtistsList(body);
+            });
+        }
+    });
+}
+
+function populateSuggestedArtistsList(body) {
+    artistOptionsData = [];
+    suggestedArtists.textContent = "Searching for: " + artistInput.value;
+    for (let i = 0; i < body.length; i++) {
+        artistOptionsData.push({
+            id: i,
+            text: body[i].name
+        });
+        let div = document.createElement("div");
+        div.textContent = artistOptionsData[i].text;
+        div.className = "suggestedArtist";
+        div.addEventListener("click", () => {   
+            artistInput.value = artistOptionsData[i].text;
+        });
+        suggestedArtists.append(div);
+    }
+    suggestedArtists.style = "block"; 
 }
 
 function initArtistSearch() {
